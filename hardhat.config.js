4de5600f3e94ebd29cb1d8bb1ee6d3b54b271a79
Anoop Kumar <anoopkumar@autorabit.com>
2021-12-23 12:26:34 +0530

    Interacting with Smart Contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,13 @@
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const {
   API_URL,
   PRIVATE_KEY
 } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.7.3",
@@ -20,4 +22,7 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`]
     }
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
